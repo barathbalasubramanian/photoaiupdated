3233f87474856b0483296a9793670b2387adb929
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2024-07-16 21:21:29 +0530

    404 in Prod Amplify ..

--- app/search/components/Search/searchpage.jsx
+++ app/search/components/Search/searchpage.jsx
@@ -185,7 +185,7 @@ export default function Search({ AllEventData, SuperAdmin }) {
     }
 
     else {
-      const response = await axios.post('api/createfolder', {
+      const response = await axios.post('https://master.d1kgp3bf18viqk.amplifyapp.com/api/createfolder', {
         folderName: folderName,
       }, {
         headers: {
